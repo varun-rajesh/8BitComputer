7f7eaabfcf207384e19e24a115c19057fa859872
varun-rajesh <varun.rajesh81@gmail.com>
2021-07-29 18:47:18 -0400

    added d flip flop

--- flops.v
+++ flops.v
@@ -1,7 +1,25 @@
-module d_ff();
+module d_ff(input clk, d, preset, clear, output q, q_bar);
 
-endmodule
+  wire master_q;
+  wire master_q_bar;
 
-module jk_ff();
+  gated_sr_latch master_gsr(
+    .enable (clk),
+    .set (d),
+    .reset (~d),
+    .preset (preset),
+    .clear (clear),
+    .q (master_q),
+    .q_bar (master_q_bar)
+  );
 
+  gated_sr_latch slave_gsr(
+    .enable (~clk),
+    .set (master_q),
+    .reset (master_q_bar),
+    .preset (preset),
+    .clear (clear),
+    .q (q),
+    .q_bar (q_bar)
+  );
 endmodule
