5aec78004360be9fd971aba5f56375bf63b26aa6
varun-rajesh <varun.rajesh81@gmail.com>
2021-08-01 16:44:08 -0400

    added quad byte mux

--- mux.v
+++ mux.v
@@ -10,3 +10,30 @@ module byte_mux(input[7:0] a, b, input sel, output[7:0] out);
     .out (out)
   );
 endmodule
+
+//sel0 = 1: a or c; sel1 = 1: a or b
+module quad_byte_mux(input[7:0] a, b, c, d, input sel0, sel1, output[7:0] out);
+  wire[7:0] mux0_p, mux1_p;
+
+  byte_mux mux0(
+    .a (a),
+    .b (b),
+    .sel (sel0),
+    .out (mux0_p)
+  );
+
+  byte_mux mux1(
+    .a (c),
+    .b (d),
+    .sel (sel0),
+    .out (mux1_p)
+  );
+
+  byte_mux mux2(
+    .a (mux0_p),
+    .b (mux1_p),
+    .sel (sel1),
+    .out (out)
+  );
+
+endmodule
