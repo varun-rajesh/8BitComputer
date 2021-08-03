fdb97bdef1683e786ddb918e42b14fcae7c5f351
varun-rajesh <varun.rajesh81@gmail.com>
2021-08-02 22:22:34 -0400

    added testbenches

--- testbench.v
+++ testbench.v
@@ -4,31 +4,34 @@
 `include "counter.v"
 `include "mux.v"
 `include "arithmetic.v"
+`include "logic.v"
+`include "alu.v"
+`include "stack.v"
+`include "memory.v"
+`include "computer.v"
 
 module testbench;
 
-  reg[7:0] a, b;
-  wire sign;
-  wire[7:0] c;
+  reg[2047:0] code;
+  reg reset, clk;
 
-
-  // always begin
-  //   #10 clk = ~clk;
-  // end
+  always begin
+    #10 clk = ~clk;
+  end
 
   initial begin
 
     $dumpfile("testbench.vcd");
     $dumpvars;
 
-    a = 8'h6f;
-    b = 8'had;
+    clk = 0;
+    code = {8'ha8, 8'h12, 8'h01, 8'h00, 8'h09, 8'h03, 8'h06, 8'h10, 8'hee, 8'h06, 8'h0f, 8'h02, 8'h00, 8'h0e, 8'h03, 8'h0c, 8'h03, 8'h42, 8'he7, 8'h08, 8'h03, 8'h00, 8'h01, 8'h03, 8'h01, 8'h0d, 8'h00, 8'h0d, 8'h01, 8'h0c, 8'h00, 8'h0c, 8'hfe, 8'h00, 8'h01, 8'h44, 8'h01, 8'h01};
+    reset = 0;
+    #1 reset = 1;
 
-    // #1 reset = 1;
-
-    #10000 $finish;
+    #4000 $finish;
   end
 
-  subtract s(a, b, c, sign);
+  computer computer(clk, reset, code);
 
 endmodule
